816322c4f8a2feecd5aa5910464e5c7c1edf9bde
Wisniewski <jwisniewski994@gmail.com>
2025-04-30 22:21:57 +0200

    -zmiana nazwy wyposazenia w migracji - zmiana pol maxtime i mintime w migracji

---
+++ RoomReservation.Infrastructure/Migrations/20250430202055_changeEquipmentName.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoomReservation.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeEquipmentName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MinTime",
+                table: "RoomsReservationLimits",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MaxTime",
+                table: "RoomsReservationLimits",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Tablica");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MinTime",
+                table: "RoomsReservationLimits",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MaxTime",
+                table: "RoomsReservationLimits",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Whiteboard");
+        }
+    }
+}
